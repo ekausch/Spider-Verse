0ce77c622fd1774b60f0a89fbc82e65c3e9c8d03
ekausch <123043069+ekausch@users.noreply.github.com>
2023-01-31 11:37:14 -0800

    Add files via upload

---
+++ SpiderVerse.R
@@ -0,0 +1,16 @@
+## Digital Humanities Librarianship Tech Tools Exploration
+## Exploring datastorage in GitHub using Spiderman Into the Spider-Verse as our data.
+
+# Loading libraries
+library(dplyr)
+library(tidyverse)
+library(tidytext)
+library(stopwords)
+
+
+
+
+
+
+
+
